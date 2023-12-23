6be12968bb63aab7757200c1468bd7a3ea6fffbd
Mauro Dinis Raimundo <140101450+MauroTwister475@users.noreply.github.com>
2023-12-23 17:42:00 +0000

    Add files via upload

--- ListContry/ContryItem/index.tsx
+++ ListContry/ContryItem/index.tsx
@@ -6,6 +6,10 @@ interface ContryProps {
 
 const Icons = [
   {
+    state: "Em abstenção",
+    icon: <Minus className="text-amber-400" />
+  },
+  {
     state: "Concorda",
     icon: <Check className="text-emerald-600" />
   },
@@ -13,21 +17,17 @@ const Icons = [
     state: "Discorda",
     icon: <X className="text-red-600" />
   },
-  {
-    state: "Em abstenção",
-    icon: <Minus className="text-amber-400" />
-  },
 ];
 
 export function ContryItem({ contryName }: ContryProps) {
   return (
-    <div className="w-full shadow2 h-14 rounded-lg bg-white border px-6 flex items-center justify-between border-violet-700">
+    <div className="w-full shadow2 h-14 rounded-lg bg-white border px-6 flex items-center justify-between ">
       <span className="text-gray-500">
         {contryName}
       </span>
       <div className="w-max flex items-center gap-4 text-gray-500">
         <select
-          className='bg-gray-50 border uppercase border-gray-300 text-gray-900 text-sm rounded-sm outline-none focus:ring-primary-600 focus:border-primary-600 block w-full p-2 focus:border-violet-700'
+          className='bg-gray-50 border uppercase border-gray-300 text-gray-900 text-sm rounded-md outline-none focus:ring-primary-600 focus:border-primary-600 block w-full p-1 focus:border-violet-700'
         >
           {Icons.map(({ icon, state }) => (
             <option key={state}>
--- ListContry/index.tsx
+++ ListContry/index.tsx
@@ -2,28 +2,39 @@
 import { TitleSection } from "@/app/components/TitleSection";
 import { PageRoot } from "@/app/components/PageRoot";
 import { ContryItem } from "./ContryItem";
+import { Form } from "../Form";
 
 const ActionsLabel = [
-  { label: "País"},
-  { label: "Posição"},
+  { label: "País" },
+  { label: "Posição" },
 ];
 
 export default function ListContry() {
 
   return (
     <PageRoot classeName="w-full">
-        <div className="w-full h-auto flex flex-col gap-2 py-8">
-          <div className="w-full flex justify-between items-center text-gray-500 px-6">
-            {ActionsLabel.map(action => (
-              <h1 key={action.label} className="font-semibold">
-                {action.label}
-              </h1>
-            ))}
-          </div>
-          <ContryItem contryName="Angola"/>
-          <ContryItem contryName="Egipto"/>
-          <ContryItem contryName="Namíbia"/>
+      <div className="w-full h-[40rem] flex flex-col gap-2 py-8">
+        <div className="w-full flex justify-between items-center text-gray-500 px-6">
+          {ActionsLabel.map(action => (
+            <h1 key={action.label} className="font-semibold">
+              {action.label}
+            </h1>
+          ))}
         </div>
+        <div className="w-full scrool flex flex-col gap-2 p-4">
+          <ContryItem contryName="Angola" />
+          <ContryItem contryName="Egipto" />
+          <ContryItem contryName="Namíbia" />
+          <ContryItem contryName="África do sul" />
+          <ContryItem contryName="Bégica" />
+          <ContryItem contryName="Estados Unidos da Ámerica" />
+        
+          <Form.Button className="w-32 ml-auto absolute bottom-5 font-semibold">
+          Ok
+        </Form.Button>
+        </div>
+        
+      </div>
     </PageRoot>
   );
 }
